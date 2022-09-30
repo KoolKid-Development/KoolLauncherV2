349781901b5d0614723e485a1d18f246ed4aff29
KoolKid <93000739+ItzKoolKid96@users.noreply.github.com>
2022-09-30 13:30:35 +0200

    Fixed some formating and added some things for dummys

--- KoolLauncher/settings.cs
+++ KoolLauncher/settings.cs
@@ -17,24 +17,24 @@
     //Hi and welcome to KoolLauncher here you can config the launcher
     //Here are some things that can help you config the launcher
     // 1 = Enable
-    // 2 = Disable
+    // 0 = Disable
     //IF YOU WANT A GOOD CONFIGURATION JOIN OUR DISCORD
     //URL: discord.kool-kid.xyz
-    //
+    //You are required to use http and https if you hae after the url
     public class settings
     {
 
-        public static string ServerName = ""; //Your server nname!
-        public static string enablemysql = "0"; //If you want to use the MySQL part whit the launcher | TO CONFIG MYSQL GO IN APP.CONFIG
-        public static string launcherregisterurl = ""; //The launcher register addres for the register option | This thing requires MySQL | the deafult path is https://yoururl.net/auth/register.php
+        public static string ServerName = "MyAwsomeServer"; //Your server nname!
+        public static string enablemysql = "0"; //If you want to use the MySQL part with the launcher | TO CONFIG MYSQL GO IN APP.CONFIG
+        public static string launcherregisterurl = "https://myserver.net/launcher/auth/register.php"; //The launcher register addres for the register option | This thing requires MySQL | the deafult path is https://yoururl.net/auth/register.php
         public static string launcherlinks = "1"; //Enable Portal Links 
-        public static string launcherdownloadlink = ""; //The link where you can download the launcher | Used for update system!
-        public static string websitelink = ""; //Your minecraft website main page link!
-        public static string forumlink = ""; //Your forum link
-        public static string storelink = ""; //Your store link
-        public static string votelink = ""; //Your vote link
-        public static string discordlink = ""; //Your discord invite link
-        public static string serverip = ""; //Your server ip!
-        public static string serverport = ""; //Your server port! 
+        public static string launcherdownloadlink = "https://myserver.net/launcher/dashboard/launcher.php"; //The link where you can download the launcher | Used for update system!
+        public static string websitelink = "https://myserver.net"; //Your minecraft website main page link!
+        public static string forumlink = "https://myserver.net/forum"; //Your forum link
+        public static string storelink = "https://store.myserver.net"; //Your store link
+        public static string votelink = "https://myserver.net/vote"; //Your server voting link link
+        public static string discordlink = "https://discord.myserver.net"; //Your discord invite link
+        public static string serverip = "play.yourserver.net"; //Your server ip!
+        public static string serverport = "25565"; //Your server port! 
     }
 }
